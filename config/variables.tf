68b4b6bb9f610952187a26d0f637cdb23ca66036
Jared Ellison <jaredellison@gmail.com>
2023-07-30 11:48:57 -0400

    build: add terraform config

---
+++ config/variables.tf
@@ -0,0 +1,19 @@
+variable "gcp_project" {
+  type = string
+}
+
+variable "gcp_region" {
+  type = string
+}
+
+variable "gcp_target_image_name" {
+  type = string
+}
+
+variable "gcp_container_registry_host" {
+  type = string
+}
+
+variable "gcp_cloud_run_service_name" {
+  type = string
+}
